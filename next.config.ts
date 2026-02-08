728e9bd0313b9499888c80a70225d8086d98761f
finleymurray <finley-murray@hotmail.com>
2026-02-08 03:52:58 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/scarepark-queue",
+  // basePath: "/scarepark-queue",  <-- DELETE THIS LINE (or comment it out like this)
   images: {
     unoptimized: true,
   },
